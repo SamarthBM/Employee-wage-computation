54cde3efbba31c49df9fab3d62fd16184c7ee1cd
SamarthBM <samarthbm18@gmail.com>
2021-06-12 13:56:01 +0530

    added wage of employee for a month

--- employee-wage-computation.sh
+++ employee-wage-computation.sh
@@ -4,7 +4,7 @@ echo "Welcome to Employee wage computation"
 attendance=$((RANDOM%3))
 
 case $attendance in
-	0)	echo "Employee is absent" 
+	0)	echo "Employee is absent"
 		 ;;
 
 	1)	echo "Employee is present full time"
@@ -16,5 +16,10 @@ case $attendance in
 		 ;;
 esac
 wage_per_hour=20
+num_of_days=20
 
-echo "Daily wage of employee is $(($wage_per_hour*$working_hour))"
+wage_per_day=$(($wage_per_hour*$working_hour))
+echo "wage per day of employee is $wage_per_day"
+
+wage_for_month=$(($wage_per_day*$num_of_days))
+echo "Wage of employee for a month is $wage_for_month"
