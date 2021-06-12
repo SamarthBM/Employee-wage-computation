bed353ff9870df75ddc66cb025934a159813e864
SamarthBM <samarthbm18@gmail.com>
2021-06-12 13:47:48 +0530

    refractor: Solving using case

--- employee-wage-computation.sh
+++ employee-wage-computation.sh
@@ -3,17 +3,18 @@ echo "Welcome to Employee wage computation"
 
 attendance=$((RANDOM%3))
 
-if [ $attendance -eq 0 ]
-then
-	echo "Employee is absent"
-elif [ $attendance -eq 1 ]
-then
-	echo "Employee is present full time"
-	working_hour=8
-else
-	echo "Employee is present for part time"
-	working_hour=4
-fi
+case $attendance in
+	0)	echo "Employee is absent" 
+		 ;;
+
+	1)	echo "Employee is present full time"
+		working_hour=8
+		 ;;
+
+	2)	echo "Employee is present for part time"
+		working_hour=4
+		 ;;
+esac
 wage_per_hour=20
 
 echo "Daily wage of employee is $(($wage_per_hour*$working_hour))"
