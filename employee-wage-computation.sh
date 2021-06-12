6012c3a206e842056cb3c4534bf907e62becff3f
SamarthBM <samarthbm18@gmail.com>
2021-06-12 13:40:07 +0530

    Add: part time present and part time wage

--- employee-wage-computation.sh
+++ employee-wage-computation.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to Employee wage computation"
 
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 
 if [ $attendance -eq 0 ]
 then
 	echo "Employee is absent"
+elif [ $attendance -eq 1 ]
+then
+	echo "Employee is present full time"
+	working_hour=8
 else
-	echo "Employee is present"
+	echo "Employee is present for part time"
+	working_hour=4
 fi
-
 wage_per_hour=20
-working_hour=8
 
-echo "Daily employee wage is $(($attendance*$wage_per_hour*$working_hour))"
+echo "Daily wage of employee is $(($wage_per_hour*$working_hour))"
